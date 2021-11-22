af0e28943e96def9336e9692f64377c41863c79f
ahas36 <ali.hassani@deakin.edu.au>
2021-11-22 20:58:49 +1100

    live coding during workshop 3

--- public/env.js
+++ public/env.js
@@ -1,27 +1,41 @@
-const testButtonFunction=()=>{
-  alert('Thank you for clicking')
-}
-
-// connect to the socket
-
 let socket = io();
-
 
 socket.on('number', (msg) => {
     console.log('Random number: ' + msg);
 })
 
-console.log('test')
-$(document).ready(function(){
-  console.log('Ready')
-  
-  //bind the button
-  $('#testButton').click(testButtonFunction)
 
-  //test get call
-  $.get('/test?user_name="Fantastic User"',(result)=>{
-    console.log(result)
-  })
+function projectCard(project) {
+    return `
+  <div class="col s6 m4 l3 xl2">
+    <div class="card">
+      <div class="card-image">
+        <img src="${project.img ? project.img : 'assets/ale.jpg'}">
+        <span class="card-title">${project.title}</span>
+      </div>
+      <div class="card-content">
+        <p>${project.info}</p>
+      </div>
+      <div class="card-action">
+        <a href="project.html?pid=${project.projectID}">This is a link</a>
+      </div>
+    </div>
+  </div>`;
+}
 
 
+$(document).ready(function() {
+    console.log('Ready')
+
+    $('.sidenav').sidenav();
+
+
+    //test get call
+    $.get('/projects', (result) => {
+        for (let p of result) {
+            $('#projects-list').append(projectCard(p))
+        }
+
+        console.log(result)
+    })
 })
--- server.js
+++ server.js
@@ -7,33 +7,50 @@ let io = require('socket.io')(http);
 
 
 
-
-
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/test", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
+
+app.get("/test", function(request, response) {
+    var user_name = request.query.user_name;
+    response.end("Hello " + user_name + "!");
 });
 
 
 //socket test
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  setInterval(()=>{
-    socket.emit('number', parseInt(Math.random()*10));
-  }, 1000);
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10));
+    }, 1000);
 
 });
 
 
-http.listen(port,()=>{
-  console.log("Listening on port ", port);
+const projects = [];
+
+for (let id = 1; id < 21; id++) {
+    projects.push({
+        projectID: id,
+        title: 'project ' + id,
+        info: `This is the info for project number ${id}`,
+        img: null,
+    });
+}
+
+
+app.get("/projects", function(request, response) {
+    response.json(projects);
+});
+
+
+
+http.listen(port, () => {
+    console.log("Listening on port ", port);
 });
 
 //this is only needed for Cloud foundry 
