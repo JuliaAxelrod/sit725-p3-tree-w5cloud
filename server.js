4019633efc521ad043a62c5fe3d9d9d0d49393b2
ahas36 <ali.hassani@deakin.edu.au>
2021-11-29 20:48:33 +1100

    add database add img upload add post add create project

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ let app = express();
 //var app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
-
+let dbo = require('./db/conn');
 
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
 
-
-app.get("/test", function(request, response) {
+app.get("/test", function (request, response) {
     var user_name = request.query.user_name;
     response.end("Hello " + user_name + "!");
 });
@@ -43,15 +43,57 @@ for (let id = 1; id < 21; id++) {
 }
 
 
-app.get("/projects", function(request, response) {
-    response.json(projects);
+app.get("/projects", function (request, response) {
+    dbo.getDB()
+        .collection("projects")
+        .find({})
+        .toArray((err, res) => {
+            if (err) {
+                throw err;
+            }
+            response.send(res);
+        })
+});
+
+
+app.get("/project", function (request, response) {
+    console.log(request.query.id);
+    dbo.getDB()
+        .collection("projects")
+        .find({projectID:request.query.id})
+        .toArray((err, res) => {
+            if (err) {
+                throw err;
+            }
+            response.send(res);
+        })
+});
+
+app.post("/project", function (request, response) {
+    //ToDo you have to add proper validation
+    if (!request.body)
+        response.sendStatus(500);
+    
+    dbo.getDB()
+        .collection("projects")
+        .insertOne(request.body);
+
+    response.sendStatus(204);
 });
 
 
 
-http.listen(port, () => {
-    console.log("Listening on port ", port);
+dbo.connect((err) => {
+    if (err) {
+        console.error(err);
+        process.exit();
+    }
+
+    http.listen(port, () => {
+        console.log("Listening on port ", port);
+    });
 });
+
 
 //this is only needed for Cloud foundry 
 require("cf-deployment-tracker-client").track();
